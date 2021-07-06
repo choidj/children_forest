570bd6f6a727f9952f6e6e4b7ba17bdcc18adb5d
choidj <69896751+choidj@users.noreply.github.com>
2021-07-06 17:53:55 +0900

    maching update

--- Assets/puzzle/Matching.cs
+++ Assets/puzzle/Matching.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Matching : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int mn_id = 0;
+    public bool mb_classifyWhetherAns = false;
+    public Sprite[] msl_changeAnsImg = new Sprite[4];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter2D(Collider2D cCollideObject) {
+        if(cCollideObject.GetComponent<Matching>().mn_id == this.mn_id) {
+            if (mb_classifyWhetherAns) {
+                gameObject.GetComponent<Image>().sprite = msl_changeAnsImg[mn_id];
+            }
+            else {
+                Destroy(this.gameObject);
+            }
+        }
+
     }
 }
