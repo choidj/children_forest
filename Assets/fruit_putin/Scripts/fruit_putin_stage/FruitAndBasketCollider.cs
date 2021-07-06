def8371284d1de51847c30c4bf825593a1711477
choidj <69896751+choidj@users.noreply.github.com>
2021-07-06 17:03:38 +0900

    add fruitputin stage

---
+++ Assets/fruit_putin/Scripts/fruit_putin_stage/FruitAndBasketCollider.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitAndBasketCollider : MonoBehaviour {
+    // When the fruit collide basket, fruit will be disappeared.
+    void OnTriggerEnter2D(Collider2D cCollideObject) {
+
+        if(cCollideObject.tag == "PutFruitInBasket")
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/fruit_putin/Scripts/fruit_putin_stage/FruitPutInStageControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FruitPutInStageControl : MonoBehaviour {
+    public GameObject mg_instanceFruit;
+    public int mn_countFruits = 10;
+    private Text mt_putFruitSize;
+    private List<GameObject> ml_fruitList = new List<GameObject>();
+    public Sprite[] msl_changeSpritesImg = new Sprite[12];
+
+    // When fruitPutIn stage start, random initializing position of fruits.
+    void Start() {
+        mt_putFruitSize = GameObject.Find("PutFruitSize").GetComponent<Text>();
+        mt_putFruitSize.text = mn_countFruits.ToString();
+        for (int i = 0; i < mn_countFruits; i++) {
+            GameObject fruit = Instantiate(mg_instanceFruit);
+            fruit.transform.position = new Vector2(Random.Range(-8f, 8f),
+                Random.Range(-4f, 4f));
+            fruit.GetComponent<SpriteRenderer>().sprite = msl_changeSpritesImg[Random.Range(0, 11)];
+            ml_fruitList.Add(fruit);
+        }
+    }
+
+    // check size of fruits... and update number of text object to n_countFruits...
+    void Update() {
+        int n_countFruits = 10;
+        for(int i = 0; i < mn_countFruits; i++) {
+            if (ml_fruitList[i] == null) {
+                n_countFruits--;
+            }
+        }
+        mt_putFruitSize.text = n_countFruits.ToString();
+    }
+}
