014af49428d1fb91ac79f1e8c5c2bfaf4b735ddc
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-07 15:21:01 +0900

    complete_matchingshape

---
+++ Assets/Shape_matching/Scripts/Drag_Shape.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag_Shape : MonoBehaviour
+{
+    private void OnMouseDrag()
+    {
+        Vector2 v_mousePosition = new Vector2(Input.mousePosition.x,
+        Input.mousePosition.y);
+        Vector2 v_worldObjPos = Camera.main.ScreenToWorldPoint(v_mousePosition);
+        this.transform.position = v_worldObjPos;
+    }
+}
