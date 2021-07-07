93d6352a48fd92eac6b57d20681c3f59608f7c0d
choidj <69896751+choidj@users.noreply.github.com>
2021-07-07 09:06:46 +0900

    update matching and create new scene

--- Assets/puzzle/Matching.cs
+++ Assets/puzzle/Matching.cs
@@ -5,14 +5,13 @@ using UnityEngine.UI;
 
 public class Matching : MonoBehaviour
 {
-    public int mn_id = 0;
     public bool mb_classifyWhetherAns = false;
     public Sprite[] msl_changeAnsImg = new Sprite[4];
 
     void OnTriggerEnter2D(Collider2D cCollideObject) {
-        if(cCollideObject.GetComponent<Matching>().mn_id == this.mn_id) {
+        if(cCollideObject.name[cCollideObject.name.Length-1] == this.name[this.name.Length - 1]) {
             if (mb_classifyWhetherAns) {
-                gameObject.GetComponent<Image>().sprite = msl_changeAnsImg[mn_id];
+                gameObject.GetComponent<Image>().sprite = msl_changeAnsImg[this.name.Length - 1];
             }
             else {
                 Destroy(this.gameObject);
