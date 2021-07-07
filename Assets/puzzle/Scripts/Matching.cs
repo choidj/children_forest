3cc9adad6b53f7b9e98d80c34a51afde42891b24
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-07 11:55:13 +0900

    complete_puzzle

--- Assets/puzzle/Scripts/Matching.cs
+++ Assets/puzzle/Scripts/Matching.cs
@@ -6,14 +6,20 @@ using UnityEngine.UI;
 public class Matching : MonoBehaviour
 {
     public bool mb_classifyWhetherAns = false;
-    public Sprite[] msl_changeAnsImg = new Sprite[4];
+    public Sprite[] msl_changeAnsImg = new Sprite[9];
 
-    void OnTriggerEnter2D(Collider2D cCollideObject) {
-        if(cCollideObject.name[cCollideObject.name.Length-1] == this.name[this.name.Length - 1]) {
-            if (mb_classifyWhetherAns) {
-                gameObject.GetComponent<Image>().sprite = msl_changeAnsImg[this.name.Length - 1];
+    void OnTriggerEnter2D(Collider2D cCollideObject)
+    {
+        if (cCollideObject.name[cCollideObject.name.Length - 1] == this.name[this.name.Length - 1])
+        {
+            if (mb_classifyWhetherAns)
+            {
+                Color tempColor = gameObject.GetComponent<SpriteRenderer>().color;
+                tempColor.a = 1f;
+                gameObject.GetComponent<SpriteRenderer>().color = tempColor;
             }
-            else {
+            else
+            {
                 Destroy(this.gameObject);
             }
         }
