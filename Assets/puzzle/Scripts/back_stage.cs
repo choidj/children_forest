1908702e22f35f9dc35fadd138b72cf4db7aa260
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-07 13:22:56 +0900

    create_back_stage.cs

---
+++ Assets/puzzle/Scripts/back_stage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class back_stage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){  //업데이트 문에다 넣어줘야함 . 
+        SceneManager.LoadScene("뒤로갈 씬 이름 "); // 씬으로 이동 .
+        //Application.Quit();  // 씬 종료 .(나가기)
+        //위씬으로 이동이나 종료기능 둘중하나 원하시는것을 사용
+  
+    }
+}
