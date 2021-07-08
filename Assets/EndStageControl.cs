9f31b530a9f0a9466998bb15b5d1e462eee313fd
choidj <69896751+choidj@users.noreply.github.com>
2021-07-08 16:41:14 +0900

    update ene_scene

--- Assets/EndStageControl.cs
+++ Assets/EndStageControl.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndStageControl : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        Invoke("nextStage", 2f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void nextStage() {
+        SceneManager.LoadScene("select_stage_scene");
     }
 }
