7e973bc2a53ab19c42ae72dea801e36d20df8291
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-09 16:09:56 +0900

    change

--- Assets/Shape/Scripts/Shape_Matching_Shape.cs
+++ Assets/Shape/Scripts/Shape_Matching_Shape.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Shape_Matching_Shape : MonoBehaviour{
-
+    AudioSource auSource;
     public bool mb_classifyWhetherAns = false; //matching되기 전
     //public Sprite[] msa_changeAnsImg = new Sprite[4];
     public Sprite sNextSprite; //빈자리에 퍼즐을 맞춘 후 정답일 때 넣을 퍼즐 조각
 
     void OnTriggerEnter2D(Collider2D cCollideObject){
         if (cCollideObject.name[cCollideObject.name.Length - 1] == this.name[this.name.Length - 1]){
-            if (mb_classifyWhetherAns)
+            if (mb_classifyWhetherAns){
                 gameObject.GetComponent<SpriteRenderer>().sprite = sNextSprite;
+                auSource.Play();
+            }
             else
                 Destroy(this.gameObject);
         }
+    }
+
+    void Awake()
+    {
+        auSource = GetComponent<AudioSource>();
     }
 }
 
