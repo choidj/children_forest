1dffeaee87234c82e4d53504bd7febb9e097855b
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-09 15:50:29 +0900

    sound

--- Assets/Puzzle/Scripts/Puzzle_Matching_Puzzle.cs
+++ Assets/Puzzle/Scripts/Puzzle_Matching_Puzzle.cs
@@ -6,10 +6,8 @@ using UnityEngine.UI;
 public class Puzzle_Matching_Puzzle : MonoBehaviour{
     public bool mb_classifyWhetherAns = false; //matching되기 전 
     //public Sprite[] msa_changeAnsImg = new Sprite[9];
-    //public AudioClip aumatching;
-    //public AudioClip aunmatching;
     AudioSource auSource;
-
+    
     private void Start(){
         if(!mb_classifyWhetherAns){
             transform.position = new Vector3(Random.Range(19, 26), Random.Range(3, 11), 0);//자리 랜덤 선정
@@ -19,24 +17,22 @@ public class Puzzle_Matching_Puzzle : MonoBehaviour{
     void OnTriggerEnter2D(Collider2D cCollideObject){
         if (cCollideObject.name[cCollideObject.name.Length - 1] == this.name[this.name.Length - 1]){//매칭퍼즐과 매칭퍼즐과모양이같은퍼즐의 이름 맨 뒤(숫자)가 같은 경우
             if (mb_classifyWhetherAns){ //정답
-                //흐렷던 퍼즐조각을 선명하게 변경
+                //흐렸던 퍼즐조각을 선명하게 변경
                 Color tempColor = gameObject.GetComponent<SpriteRenderer>().color;
                 tempColor.a = 1f;
                 gameObject.GetComponent<SpriteRenderer>().color = tempColor;
                 auSource.Play();
             }
             else{ //오류
-
                 Destroy(this.gameObject);
                 //Invoke("Awake", 1f); //Awake함수 호출
+
             }
         }
     }
 
-    void Awake()
-        {
-            auSource = GetComponent<AudioSource>();
-        }
-
+    void Awake(){
+        auSource = GetComponent<AudioSource>();
+    }
 
 }
