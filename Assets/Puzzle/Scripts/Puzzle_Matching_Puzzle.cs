daaf24f372d7f147e3a1d9776eebf7724b69cc4c
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-09 15:28:57 +0900

    apply_sounds

--- Assets/Puzzle/Scripts/Puzzle_Matching_Puzzle.cs
+++ Assets/Puzzle/Scripts/Puzzle_Matching_Puzzle.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class Puzzle_Matching_Puzzle : MonoBehaviour{
     public bool mb_classifyWhetherAns = false; //matching되기 전 
     //public Sprite[] msa_changeAnsImg = new Sprite[9];
-    public AudioClip audiomatching;
-    public AudioClip audiounmatching;
-    AudioSource audioSource;
+    //public AudioClip aumatching;
+    //public AudioClip aunmatching;
+    AudioSource auSource;
 
     private void Start(){
         if(!mb_classifyWhetherAns){
@@ -23,8 +23,7 @@ public class Puzzle_Matching_Puzzle : MonoBehaviour{
                 Color tempColor = gameObject.GetComponent<SpriteRenderer>().color;
                 tempColor.a = 1f;
                 gameObject.GetComponent<SpriteRenderer>().color = tempColor;
-
-                Invoke("Awake", 1f); //Awake함수 호출
+                auSource.Play();
             }
             else{ //오류
 
@@ -36,6 +35,8 @@ public class Puzzle_Matching_Puzzle : MonoBehaviour{
 
     void Awake()
         {
-            this.audioSource = GetComponent<AudioSource>();
+            auSource = GetComponent<AudioSource>();
         }
+
+
 }
--- Assets/Shape/Scripts/Shape_Matching_Shape.cs
+++ Assets/Shape/Scripts/Shape_Matching_Shape.cs
@@ -8,14 +8,22 @@ public class Shape_Matching_Shape : MonoBehaviour{
     public bool mb_classifyWhetherAns = false; //matching되기 전
     //public Sprite[] msa_changeAnsImg = new Sprite[4];
     public Sprite sNextSprite; //빈자리에 퍼즐을 맞춘 후 정답일 때 넣을 퍼즐 조각
+    AudioSource auSource;
 
     void OnTriggerEnter2D(Collider2D cCollideObject){
         if (cCollideObject.name[cCollideObject.name.Length - 1] == this.name[this.name.Length - 1]){
-            if (mb_classifyWhetherAns)
+            if (mb_classifyWhetherAns){
                 gameObject.GetComponent<SpriteRenderer>().sprite = sNextSprite;
+                auSource.Play();
+            }
             else
                 Destroy(this.gameObject);
         }
+    }
+
+    void Awake()
+    {
+        auSource = GetComponent<AudioSource>();
     }
 }
 
