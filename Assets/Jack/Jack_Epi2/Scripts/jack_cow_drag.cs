6fbafda598607cf4103e61a4dff1150068743862
RyuSion1124 <86770691+RyuSion1124@users.noreply.github.com>
2021-07-14 15:34:49 +0900

    류시온 유니티 첫경험

---
+++ Assets/Jack/Jack_Epi2/Scripts/jack_cow_drag.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jack_cow_drag : MonoBehaviour
+{
+    private void OnMouseDrag()
+    {
+        Vector2 v2_checkMousePos = new Vector2(Input.mousePosition.x,
+        Input.mousePosition.y);
+        Vector2 v2_checkworldObjPos = Camera.main.ScreenToWorldPoint(v2_checkMousePos);
+        this.transform.position = v2_checkworldObjPos;
+    }
+}
