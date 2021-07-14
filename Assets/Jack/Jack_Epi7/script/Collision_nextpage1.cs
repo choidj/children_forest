2dc2645741a71fd343ab5f9b321de932f5471190
RyuSion1124 <86770691+RyuSion1124@users.noreply.github.com>
2021-07-14 16:23:05 +0900

    ㅓㅏㅓㅣㅓㅣ

---
+++ Assets/Jack/Jack_Epi7/script/Collision_nextpage1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collision_nextpage1 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D cCollideObject)
+    {
+        SceneManager.LoadScene("end_scene");
+    }
+}
