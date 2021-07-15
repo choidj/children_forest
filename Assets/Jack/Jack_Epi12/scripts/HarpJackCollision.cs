e5578b2d9e64a71b2e946c3cf312b506b7a40cec
RyuSion1124 <86770691+RyuSion1124@users.noreply.github.com>
2021-07-15 15:28:13 +0900

    3:27
    류시온 12번 시나리오 다음 신으로 이어지게 만들면 끝

--- Assets/Jack/Jack_Epi12/scripts/HarpJackCollision.cs
+++ Assets/Jack/Jack_Epi12/scripts/HarpJackCollision.cs
@@ -5,8 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class HarpJackCollision: MonoBehaviour
 {
+    public GameObject mg_talk_Prefab;
+
+
     void OnTriggerEnter2D(Collider2D cCollideObject)
     {
-        
+        GameObject g_talk = Instantiate(mg_talk_Prefab) as GameObject;
     }
 }
