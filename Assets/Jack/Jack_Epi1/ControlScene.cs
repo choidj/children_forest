cfee2b5da6aed73b5ef47e366679acc81762fdbd
choidj <69896751+choidj@users.noreply.github.com>
2021-07-15 10:59:08 +0900

    update scene

---
+++ Assets/Jack/Jack_Epi1/ControlScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            clickedMouse();
+        }
+    }
+    void clickedMouse() {
+        SceneManager.LoadScene("Jack_Epi2");
+    }
+}
