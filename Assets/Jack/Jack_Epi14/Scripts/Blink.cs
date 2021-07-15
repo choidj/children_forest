c287c7b6896d1c22e06fa9ec8ec6f2fc5824558c
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-15 14:30:46 +0900

    upload

---
+++ Assets/Jack/Jack_Epi14/Scripts/Blink.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    float f_time;
+    float timer;
+    float waitingTime;
+    void Start(){
+        timer = 0.0f;
+        waitingTime = 3.8f;
+    }
+    public void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > waitingTime){
+            if (f_time < 0.5f){
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            }
+            else{
+                GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+                if (f_time > 1f)
+                    f_time = 0;
+            }
+            f_time += Time.deltaTime;
+        }
+    }
+}
