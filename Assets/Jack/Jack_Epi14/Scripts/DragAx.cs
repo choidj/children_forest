0129175add6bf93ed8c54448d4966d4be82e75c2
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-15 15:45:10 +0900

    도끼옮기기

--- Assets/Jack/Jack_Epi14/Scripts/DragAx.cs
+++ Assets/Jack/Jack_Epi14/Scripts/DragAx.cs
@@ -6,9 +6,17 @@ public class DragAx : MonoBehaviour
 {   
     public GameObject Jack;
     public GameObject Ax;
-    public void OnMouseDrag(){
+    SpriteRenderer rend;
+        public void OnMouseDrag(){
         Vector2 v2mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 v2worldObjPos = Camera.main.ScreenToWorldPoint(v2mousePosition);
         this.transform.position = v2worldObjPos;
+    }
+    void OnTriggerEnter2D(Collider2D cCollideObject){
+        if (cCollideObject.tag == "Jack"){
+            rend =  Jack.GetComponent<SpriteRenderer>();
+            rend.flipX = false;
+            
+        }
     }
 }
