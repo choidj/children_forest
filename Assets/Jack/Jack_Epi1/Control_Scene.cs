5418b8ac170aa2af5e28fae7c0df7d102c0fd4c4
choidj <69896751+choidj@users.noreply.github.com>
2021-07-16 14:03:11 +0900

    connect scene

--- Assets/Jack/Jack_Epi1/Control_Scene.cs
+++ Assets/Jack/Jack_Epi1/Control_Scene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ControlScene : MonoBehaviour
+public class Control_Scene : MonoBehaviour
 {
     public string ms_loadScene;
     // Update is called once per frame
@@ -11,10 +11,17 @@ public class ControlScene : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
-            clickedMouse();
+            Ray ra_checkMouseDistance = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D h_hitDistanceCast2D = Physics2D.GetRayIntersection(ra_checkMouseDistance, Mathf.Infinity);
+
+            if (h_hitDistanceCast2D.collider != null && h_hitDistanceCast2D.collider.tag == "Button")
+            {
+                clickedMouse();
+            }
         }
     }
     void clickedMouse() {
         SceneManager.LoadScene(ms_loadScene);
     }
+    
 }
