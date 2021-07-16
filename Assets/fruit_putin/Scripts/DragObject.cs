4166f91e673d8b54658e77aed1ad10268d7f9411
choidj <69896751+choidj@users.noreply.github.com>
2021-07-16 13:24:52 +0900

    modify TTS

--- Assets/fruit_putin/Scripts/DragObject.cs
+++ Assets/fruit_putin/Scripts/DragObject.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class DragObject : MonoBehaviour {
     private TTS mtts_tellFruitName;
+    private AudioSource mas_playFruitNameVoice;
+    private bool mb_checkClickOnce = false;
+    void Start() {
+        mtts_tellFruitName = TTS.GetInstance(Voice.KR_FEMALE_A);
+        mas_playFruitNameVoice = gameObject.GetComponent<AudioSource>();
+    }
     private void OnMouseDrag() {
+        if(!mb_checkClickOnce) {
+            mtts_tellFruitName.CreateAudio("사과", mas_playFruitNameVoice);
+            mb_checkClickOnce = true;
+        }
         Vector2 v2_checkMousePos = new Vector2(Input.mousePosition.x,
         Input.mousePosition.y);
         Vector2 v2_checkworldObjPos = Camera.main.ScreenToWorldPoint(v2_checkMousePos);
         this.transform.position = v2_checkworldObjPos;
+    }
+    void OnMouseUp() {
+        mb_checkClickOnce = false;
     }
 }
