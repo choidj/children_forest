cb5903144f82d7a004918f590728db0c87ff2287
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-16 13:24:53 +0900

    epi14 스크립트 설명

--- Assets/Jack/Jack_Epi14/Scripts/MoveMom.cs
+++ Assets/Jack/Jack_Epi14/Scripts/MoveMom.cs
@@ -1,37 +1,50 @@
+/*
+  * - Name : Movement_Giant.cs
+  * - Writer : 이윤교
+  * - Content : 잭과콩나무 에피소드14 - 엄마 이동 스크립트
+  * 
+  *            -작성 기록-
+  *            2021-07-15 : 제작 완료
+  *
+  * MoveTowards() : 등속 이동, 매개변수로 {현재위치, 목표위치, 속도}를 입력  
+  * OnTriggerEnter2D(Collider2D cCollideObject) :오브젝트간 충돌이 일어날때 처음 한번만 호출되는 함수
+  */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveMom : MonoBehaviour
 {
-    GameObject Ax, Jack, Mom;
-    Vector3 MomPos;
+    GameObject mg_Ax, mg_Jack, mg_Mom; //게임 오브젝트 도끼 잭 엄마 선언
+    Vector3 v3_MomPos; //엄마위치저장
 
-    float timer;
-    float waitingTime;
+    float mf_timer; //현재위치
+    float mf_waitingTime; //원하는 시간
 
     void Start() {
-        timer = 0.0f;
-        waitingTime = 2f;
+        mf_timer = 0.0f;
+        mf_waitingTime = 2f; //2초뒤
         
-        Ax = GameObject.Find("Ax");
-        Jack = GameObject.Find("Jack");
-        Mom = GameObject.Find("Mom");
-        MomPos = new Vector3(-3,-1.14f,0);
+        mg_Ax = GameObject.Find("Ax"); //Ax 게임 오브젝트를 mg_Ax 변수에 저장
+        mg_Jack = GameObject.Find("Jack");//Jack 게임 오브젝트를 mg_Jack 변수에 저장
+        mg_Mom = GameObject.Find("Mom");//Mom 게임 오브젝트를 mg_Mom 변수에 저장
+        v3_MomPos = new Vector3(-3,-1.14f,0); //v3_MomPos에 엄마위치저장
     }
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer > waitingTime){
-            Mom.transform.position = Vector3.MoveTowards(Mom.transform.position, MomPos, 2f * Time.deltaTime);
+        /*2초뒤 엄마 등장*/
+        mf_timer += Time.deltaTime;
+        if (mf_timer > mf_waitingTime){
+            mg_Mom.transform.position = Vector3.MoveTowards(mg_Mom.transform.position, v3_MomPos, 2f * Time.deltaTime);
         }
         
     }
     void OnTriggerEnter2D(Collider2D cCollideObject){
-        if (cCollideObject.tag == "Mom"){
+        if (cCollideObject.tag == "Mom"){ //엄마가 등장하게 되면 도끼 생성
             Color tempColor = Ax.GetComponent<SpriteRenderer>().color;
             tempColor.a = 1f;
-            Ax.GetComponent<SpriteRenderer>().color = tempColor;
+            mg_Ax.GetComponent<SpriteRenderer>().color = tempColor;
         }
     }
 }
