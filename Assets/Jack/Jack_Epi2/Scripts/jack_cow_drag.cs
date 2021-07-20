24e184d4e3965c96728922bf8fc39d5380196303
RyuSion1124 <86770691+RyuSion1124@users.noreply.github.com>
2021-07-20 11:52:00 +0900

    류시온 스크립트 주석 처리 수정

--- Assets/Jack/Jack_Epi2/Scripts/jack_cow_drag.cs
+++ Assets/Jack/Jack_Epi2/Scripts/jack_cow_drag.cs
@@ -1,3 +1,13 @@
+﻿/*
+ * - Name : jack_cow_drag.cs
+ * - Writer : 류시온
+ * - Content : 잭과콩나무 에피소드2 - 잭,소 드래그 스크립트
+ * -기록-
+ * 2021-07-20 : 작성
+ * OnTriggerEnter2D(Collider2D cCollideObject) :오브젝트간 충돌이 일어날때 처음 한번만 호출되는 함수
+ * OnMouseDrag() : 게임오브젝트를 마우스 드래그로 이동시키는 함수
+ * 
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
