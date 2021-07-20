a24fddda6863207c4c00f544603e3382797a2792
choidj <69896751+choidj@users.noreply.github.com>
2021-07-20 10:25:22 +0900

    add pitch to set in inspector..

--- Assets/TTS.cs
+++ Assets/TTS.cs
@@ -15,10 +15,10 @@
 *            2021-07-20 : 주석 처리
  * - TTS Member Variable 
  * ms_useApiURL : Google TTS API 서버와 통신을 위한 URL 주소이다.
- * mstts_setTtsApi : Google TTS API 서버와 통신하여 데이터를 주고 받기 위한 데이터 형식을 맞춰주는 클래스이다. 이 안에는 보이스의 종류, 음조, 음성으로 바꿀 텍스트 등 음서으로 바꾸기 위해서 필요로 하는 세팅 데이터가 설정된다.
+ * mstts_setTtsApi : Google TTS API 서버와 통신하여 데이터를 주고 받기 위한 데이터 형식을 맞춰주는 클래스이다. 이 안에는 보이스의 종류, 음조, 음성으로 바꿀 텍스트 등 음서으로 바꾸기 위해서 필요로 하는 세팅 데이터가 설정된다. 이때 이 세팅을 저장하는 이너클래스가 존재하는데, 각각 SetTextToSpeech, 
  * instance : 이 함수는 아무래도 통신을 하는 클래스로써, 무겁다고 작성자가 판단이 되어 클래스의 인스턴스를 계속 생산하는 것이 아니라, 싱글톤 디자인 패턴을 이용하여 하나의 인스턴스만 생성하게 만들었다. 이렇게 하면, 클래스의 인스턴스를 하나만 생성하여 여러 오브젝트 클래스들이 재사용할 수 있게 된다.
  * 
- * -Function
+ * - TTS Member Function
  * v_NoneScript() : 스크립트를 공백으로 설정해준다.
  * v_NextScript() : 다음 스크립트를 보여준다.
  * 
@@ -71,7 +71,7 @@ public class TTS {
     public class SetAudioConfig {
         public string audioEncoding;
         public float speakingRate;
-        public int pitch;
+        public float pitch;
         public int volumeGainDb;
     }
 
@@ -85,16 +85,8 @@ public class TTS {
     private static TTS instance = null;
     //constructor initialize these class..
     //initialize the json object config to send a google mstts_setTtsApi api.
-    public TTS()
-    {
+    public TTS() {
         mstts_setTtsApi = new SetTextToSpeech();
-
-        SetAudioConfig sa_setAudioConf = new SetAudioConfig();
-        sa_setAudioConf.audioEncoding = "LINEAR16";
-        sa_setAudioConf.speakingRate = 0.8f;
-        sa_setAudioConf.pitch = 0;
-        sa_setAudioConf.volumeGainDb = 0;
-        mstts_setTtsApi.audioConfig = sa_setAudioConf;
     } 
 
     public static TTS GetInstance() {
@@ -106,12 +98,11 @@ public class TTS {
         return instance;
     }
     //convert the received byte array to float array...
-    public AudioClip CreateAudio(string sTargetSpeech,  Voice vTargetVoice) {
-        SetInput si_setInputData = new SetInput();
-        si_setInputData.text = sTargetSpeech;
-        mstts_setTtsApi.input = si_setInputData;
+    public AudioClip CreateAudio(string sTargetSpeech, Voice vTargetVoice, float fSetPitch = 0f, float fSpeakRate = 0.8f) {
         
+        setAudioConfig(fSetPitch, fSpeakRate);
         setVoice(vTargetVoice);
+        setInput(sTargetSpeech);
 
         //After request HttpWebRequest, save the returned data in string format.
         var s_str = TextToSpeechPost(mstts_setTtsApi);
@@ -179,7 +170,20 @@ public class TTS {
             return null;
         }
     }
-    void setVoice(Voice srcVoice) {
+    private void setInput(string sTargetSpeech) {
+        SetInput si_setInputData = new SetInput();
+        si_setInputData.text = sTargetSpeech;
+        mstts_setTtsApi.input = si_setInputData;
+    }
+    private void setAudioConfig(float fSetPitch, float fSpeakRate) {
+        SetAudioConfig sa_setAudioConf = new SetAudioConfig();
+        sa_setAudioConf.audioEncoding = "LINEAR16";
+        sa_setAudioConf.speakingRate = fSpeakRate;
+        sa_setAudioConf.pitch = fSetPitch;
+        sa_setAudioConf.volumeGainDb = 0;
+        mstts_setTtsApi.audioConfig = sa_setAudioConf;
+    }
+    private void setVoice(Voice srcVoice) {
         SetVoice sv_setVoiceConf = new SetVoice();
         switch(srcVoice) {
             case Voice.KR_FEMALE_A:
