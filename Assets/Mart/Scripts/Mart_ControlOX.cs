f324de6fb1e261d900f19e6240b3b01f1cb5725d
kimmhyeon <kimmhyeon@gmail.com>
2021-07-21 10:18:45 +0900

    210721 마트게임 - 정답 또는 오답 처리시 음성기능 추가

--- Assets/Mart/Scripts/Mart_ControlOX.cs
+++ Assets/Mart/Scripts/Mart_ControlOX.cs
@@ -9,10 +9,12 @@
  * 2021-07-08 : 제작 완료
  * 2021-07-09 : 코드 정리
  * 2021-07-20 : 인코딩형식 수정 및 주석 수정
+ * 2021-07-21 : 정답 또는 오답시 음성기능 추가
  *                  
  * - Variable 
  * mg_O : O 프리팹을 연결해주는 변수
  * mg_X : X 프리팹을 연결해주는 변수
+ * vm : TTS 오브젝트 연결을 위한 변수
  * 
  * -Function()
  * v_ShowO() : O 를 보여주는 스크립트
@@ -26,9 +28,10 @@ using UnityEngine;
 public class Mart_ControlOX : MonoBehaviour{
     public GameObject mg_O;
     public GameObject mg_X;
+    VoiceManager vm;
 
     void Start(){
-        
+        this.vm = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();     // TTS 오브젝트 연결
     }
 
     void Update(){
@@ -44,6 +47,7 @@ public class Mart_ControlOX : MonoBehaviour{
         GameObject show = Instantiate(mg_O) as GameObject;
         show.transform.position = new Vector3(0, 0, 0);
         Debug.Log("mg_O이미지 생성");
+        vm.playVoice(0);                                            // TTS 작동
         Destroy(show, 1);                                           // 이미지를 띄우고 삭제하는 텀을 변경하고 싶으면 이 부분 수정
         Debug.Log("mg_O이미지 삭제");
     }
@@ -56,6 +60,7 @@ public class Mart_ControlOX : MonoBehaviour{
         GameObject show = Instantiate(mg_X) as GameObject;
         show.transform.position = new Vector3(0, 0, 0);
         Debug.Log("mg_X이미지 생성");
+        vm.playVoice(1);                                            // TTS 작동
         Destroy(show,1);                                            // 이미지를 띄우고 삭제하는 텀을 변경하고 싶으면 이 부분 수정
         Debug.Log("mg_X이미지 삭제");
     }
