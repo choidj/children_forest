068eb68efcbd8c1d8a04904cd3bc61b6a009cbf5
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-21 14:31:16 +0900

    PuzzleScript 수정

--- Assets/Shape/Scripts/Shape_Matching_Shape.cs
+++ Assets/Shape/Scripts/Shape_Matching_Shape.cs
@@ -20,29 +20,23 @@ public class Shape_Matching_Shape : MonoBehaviour{
     //public Sprite[] msa_changeAnsImg = new Sprite[4];
     public Sprite sNextSprite; //빈자리에 퍼즐을 맞춘 후 정답일 때 넣을 퍼즐 조각
 
+    void Start() {
+        mv2_initPos = this.transform.position;
+    }
+    void Update() {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, mv2_initPos, 2f * Time.deltaTime);
+    }
     void OnTriggerStay2D(Collider2D cCollideObject){
         if(Input.GetMouseButtonUp(0)) {
             if (cCollideObject.name[cCollideObject.name.Length - 1] == this.name[this.name.Length - 1]){
                 if (mb_classifyWhetherAns){
                     gameObject.GetComponent<SpriteRenderer>().sprite = sNextSprite;
-                    auSource.Play();
                 }
                 else
                     Destroy(this.gameObject);
             }
 
         }
-    }
-
-    void Update() {
-        this.transform.position = Vector3.MoveTowards(this.transform.position, mv2_initPos, 2f * Time.deltaTime);
-    }
-    void Start() {
-        mv2_initPos = this.transform.position;
-    }
-    void Awake()
-    {
-        auSource = GetComponent<AudioSource>();
     }
 }
 
