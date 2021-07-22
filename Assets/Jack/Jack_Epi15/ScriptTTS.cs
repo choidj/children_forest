552007d9d88f6cdfdabdf90f6bdbd6e61101d843
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-07-22 14:18:04 +0900

    210722Jack_Epi15 스크립트 및 tts 재적용

--- Assets/Jack/Jack_Epi15/ScriptTTS.cs
+++ Assets/Jack/Jack_Epi15/ScriptTTS.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class ScriptTTS : MonoBehaviour
 {
     VoiceManager vm;
+    bool mb_checkPlayOnce = true;
 
     // Start is called before the first frame update
     void Start()
     {
         this.vm = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
-        vm.playVoice(0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(vm.mb_checkSceneReady) {
+            if(mb_checkPlayOnce) {
+                vm.playVoice(0);
+                mb_checkPlayOnce = false;
+            }
+        }
         
     }
 }
